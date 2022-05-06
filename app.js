5ac5d047503057ea1d7f06007f45e12c5cef1323
danrungg <daniel.rungger@outlook.com>
2022-05-06 21:36:42 +0200

    add a function that plays a single round

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-const myArray = ["Rock", "Paper", "Scissors"];
+const myArray = ["rock", "paper", "scissors"];
 
 const computerPlay = () => {
     return myArray[Math.floor(Math.random() * 3)];
 };
 
-console.log(computerPlay());
+const playRound = (playerSelection, computerSelection) => {
+    if (playerSelection === computerSelection) {
+        return "TIE";
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+        return "WIN";
+    } else {
+        return "LOOSE";
+    }
+};
+
+playerSelection = "paper".toLowerCase();
+computerSelection = computerPlay();
+console.log(computerSelection);
+
+console.log(playRound(playerSelection, computerSelection));
