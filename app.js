714bc429ff6b010fa2ad49055940a47f5b60cc42
danrungg <daniel.rungger@outlook.com>
2022-05-16 19:03:48 +0200

    add event listener and selection buttons

--- app.js
+++ app.js
@@ -6,14 +6,24 @@ const computerPlay = () => {
 
 const playRound = (playerSelection, computerSelection) => {
     if (playerSelection === computerSelection) {
+        console.log("tie", computerSelection);
         return "tie";
     } else if (
         (playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "scissors" && computerSelection === "paper")
     ) {
+        console.log("win", computerSelection);
         return "playerWin";
     } else {
+        console.log("loose", computerSelection);
         return "computerWin";
     }
 };
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((item) => {
+    item.addEventListener("click", function () {
+        playRound(this.innerText.toLowerCase(), computerPlay());
+    });
+});
