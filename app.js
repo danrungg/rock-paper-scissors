c5db8f2602505bde343a08ca83b44a13eaf8edc0
danrungg <daniel.rungger@outlook.com>
2022-05-16 12:54:09 +0200

    cut logic that plays five rounds

--- app.js
+++ app.js
@@ -17,38 +17,3 @@ const playRound = (playerSelection, computerSelection) => {
         return "computerWin";
     }
 };
-
-const game = () => {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt(`??? ${i + 1} of 5`).toLowerCase();
-        console.log(playerSelection);
-        let computerSelection = computerPlay();
-        console.log(`The computer choose ${computerSelection}`);
-
-        if (playRound(playerSelection, computerSelection) === "playerWin") {
-            playerScore += 1;
-            console.log(
-                `Playerscore: ${playerScore}, ComputerScore: ${computerScore}`
-            );
-        } else if (
-            playRound(playerSelection, computerSelection) === "computerWin"
-        ) {
-            computerScore += 1;
-            console.log(
-                `Playerscore: ${playerScore}, ComputerScore: ${computerScore}`
-            );
-        } else {
-            console.log("TIE!");
-        }
-    }
-
-    if (playerScore > computerScore) {
-        return "You won!";
-    } else {
-        return "The Computer won!";
-    }
-};
-
-console.log(game());
